3c251851e2dd0411cc06a1b60c2da3908f87e7be
Rajeshkarasala <107117169+Rajeshkarasala@users.noreply.github.com>
2022-06-09 14:57:16 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -6,7 +6,7 @@ var updateCallback = function(data){
     console.log("line : "+line);
     var movieName = line.text;
 	if (line.source.toLowerCase()==="visitor"){
-    var url = "https://www.omdbapi.com?t="+movieName+"&apikey=526d7c29";
+    var url = "https://www.omdbapi.com?t="+movieName+"&apikey=d9dc4a5e";
     
    
     
